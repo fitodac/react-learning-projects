5f69f401f7109f98af22c8ae4523027e4a43fbe2
fitodac <fitodac@gmail.com>
2023-10-11 02:50:06 -0300

    heores pages

---
+++ 01-heroes-spa/src/heroes/pages/DcPage.jsx
@@ -0,0 +1,13 @@
+import { HeroesList } from '../components';
+
+
+export const DcPage = () => {
+  return (
+    <>
+      <h1>DC Comics</h1>
+      <hr />
+
+      <HeroesList publisher='DC Comics' />
+    </>
+  )
+}
---
+++ 01-heroes-spa/src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,13 @@
+import { HeroesList } from '../components';
+
+export const MarvelPage = () => {
+  return (
+    <>
+      <h1>Marvel Comics</h1>
+      <hr />
+
+      <HeroesList publisher='Marvel Comics' />
+
+    </>
+  )
+}
